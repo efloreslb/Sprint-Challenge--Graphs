a6eb56c4edb1acb4d976013c7d0ca96d19773763
Edgar F <eflores.ef@gmail.com>
2020-02-24 10:16:05 -0800

    Adding rooms per untraveled direction

--- adv.py
+++ adv.py
@@ -5,30 +5,6 @@ from world import World
 import random
 from ast import literal_eval
 
-# Load world
-world = World()
-
-
-# You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
-
-# Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
-world.load_graph(room_graph)
-
-# Print an ASCII map
-world.print_rooms()
-
-player = Player(world.starting_room)
-
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
-
 class Queue():
     def __init__(self):
         self.queue = []
@@ -61,19 +37,87 @@ class Graph:
 
     def add_vertex(self, vertex_id):
         if vertex_id not in self.vertices:
-            self.vertices[vertex_id] = set()
+            # self.vertices[vertex_id] = set()
+            self.vertices[vertex_id] = {d: '?' for d in player.current_room.get_exits()}
 
     def add_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
 
-world_graph = Graph()
-print(world.rooms.values)
+# Load world
+world = World()
 
-for room in world.rooms.values():
-    world_graph.add_vertex(room)
 
-print(world_graph.vertices)
+# You may uncomment the smaller graphs for development and testing purposes.
+# map_file = "maps/test_line.txt"
+# map_file = "maps/test_cross.txt"
+map_file = "maps/test_loop.txt"
+# map_file = "maps/test_loop_fork.txt"
+# map_file = "maps/main_maze.txt"
+
+# Loads the map into a dictionary
+room_graph=literal_eval(open(map_file, "r").read())
+world.load_graph(room_graph)
+
+# Print an ASCII map
+world.print_rooms()
+
+player = Player(world.starting_room)
+
+# Fill this out with directions to walk
+# traversal_path = ['n', 'n']
+# traversal_path = ['s', 's', 'w', 'w', 'n', 'n']
+traversal_path = []
+
+gr = Graph()
+visited = set()
+
+# for room in world.rooms.values():
+#     gr.add_vertex(room)
+
+print(len(room_graph))
+print(len(gr.vertices))
+
+# while len(gr.vertices) < len(room_graph):
+print(player.current_room.id)
+if player.current_room.id not in gr.vertices:
+    gr.add_vertex(player.current_room.id)
+    visited.add(player.current_room.id)
+    # print(gr.vertices[player.current_room.id])
+    # print(visited)
+
+for direction in gr.vertices[player.current_room.id]:
+    print(f'current room: {player.current_room.id}')
+    print(gr.vertices)
+    if gr.vertices[player.current_room.id][direction] == "?":
+        player.travel(direction)
+
+        # wander
+        if player.current_room.id not in gr.vertices:
+            gr.add_vertex(player.current_room.id)
+            visited.add(player.current_room.id)
+
+
+
+    
+
+
+# for direction in graph.vertices[0].items():
+#     # if graph.vertices[0] == '?':
+#     #     print("found ?")
+#     print(direction)
+
+# print(player.current_room.id)
+# player.travel('n')
+# print(player.current_room.id)
+# player.travel('n')
+# print(player.current_room.id)
+# print(player.current_room.get_exits())
+# print(f'vertices {graph.vertices}')
+
+
+
+
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -89,8 +133,6 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
-
 
 #######
 # UNCOMMENT TO WALK AROUND
